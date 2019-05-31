0901e4126413c471483f61000f9b2d93306b8dc7
in_vane <in_vane@yuhu.tech>
2019-05-31 17:48:30 +0800

    feat: add chain && v1.0.0

--- pages/h2-order/list-order-detail/list-order-detail.js
+++ pages/h2-order/list-order-detail/list-order-detail.js
@@ -18,7 +18,12 @@ Page({
       []
     ],
     multiIndex: [0, 0],
+    ptname: '',
+    agent_list: [],
+    agentid_list: [],
     regMode: '',
+    type: '',
+    agentid: '',
     avatar: ''
   },
 
@@ -65,7 +70,14 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
-
+    wx.showNavigationBarLoading();
+    this.setData({
+      type: '',
+      agentid: '',
+      regMode: '',
+      ptname: ''
+    })
+    this.doSearch()
   },
 
   /**
@@ -87,6 +99,9 @@ Page({
       query: `query {
         search(
           orderid: "${this.data.orderid}"
+          ${this.data.ptname ? `ptname:"${this.data.ptname}"`:''}
+          ${this.data.type?`type:${this.data.type}`:''}
+          ${this.data.agentid?`inviterid:"${this.data.agentid}"`:''}
         ) {
           originorder{
             orderid
@@ -146,11 +161,15 @@ Page({
       let temp_list = []
       let temp_ing = []
       let temp_wait = []
+      let temp_name = []
+      let temp_id = []
       let avatar = util.selectAvatar(res.search[0].originorder.occupation)
       util.formatItemOrigin(res.search[0])
+      /* 处理时间 */
       if (res.search[0].modifiedorder.length > 0) {
         util.formatItemModify(res.search[0])
       }
+      /* 处理PT */
       if (res.search[0].pt && res.search[0].pt.length > 0) {
         for (let item of res.search[0].pt) {
           if (item.remark) {
@@ -167,9 +186,16 @@ Page({
           }
         }
       }
+      /* 处理代理 */
+      if (res.search[0].agent) {
+        for (let item of res.search[0].agent) {
+          temp_name.push(item.name)
+          temp_id.push(item.agentid)
+        }
+      }
       if (temp_list.length === 0 && temp_ing.length === 0 && temp_wait.length === 0) {
         wx.showToast({
-          title: '还没有人报名',
+          title: '无结果',
           icon: 'none'
         })
       }
@@ -179,9 +205,11 @@ Page({
         pt_list_wait: temp_wait,
         pt_list_ing: temp_ing,
         avatar: avatar,
-        ['multiArray[1]']: res.search[0].agent,
-        agent_list: res.search[0].agent
+        agent_list: temp_name,
+        agentid_list: temp_id
       })
+      wx.hideNavigationBarLoading();
+      wx.stopPullDownRefresh();
     }).catch((error) => {
       console.log('fail', error);
       wx.showToast({
@@ -199,11 +227,16 @@ Page({
 
   doSearchName: function(e) {
     console.log(e)
+    this.setData({
+      ptname: e.detail.value
+    })
     gql.query({
       query: `query {
         search(
           orderid:"${this.data.orderid}"
           ${e.detail.value ? `ptname: "${e.detail.value}"` : ''}
+          ${this.data.type ? `type:${this.data.type}` : ''}
+          ${this.data.agentid ? `inviterid:"${this.data.agentid}"` : ''}
         ) {
           pt{
             ptid
@@ -280,7 +313,30 @@ Page({
     this.setData({
       multiIndex: e.detail.value
     })
-    console.log('改变了值', this.data.multiIndex)
+    switch (this.data.multiIndex[0]) {
+      case 0:
+        this.setData({
+          regMode: '自由报名',
+          agentid: '',
+          type: 1
+        })
+        break
+      case 1:
+        this.setData({
+          regMode: '顾问端分享',
+          agentid: '',
+          type: 2
+        })
+        break
+      case 2:
+        this.setData({
+          regMode: this.data.agent_list[this.data.multiIndex[1]],
+          agentid: this.data.agentid_list[this.data.multiIndex[1]],
+          type: 3
+        })
+        break
+    }
+    this.doSearch()
   },
 
   bindMultiPickerColumnChange(e) {
@@ -291,19 +347,13 @@ Page({
       case 0:
         switch (multiIndex[0]) {
           case 0:
-            multiArray[1] = this.data.agent_list
+            multiArray[1] = []
             break
           case 1:
-            multiArray[1] = this.data.pt_list
+            multiArray[1] = []
             break
           case 2:
-            multiArray[1] = []
-            break
-          case 3:
-            multiArray[1] = []
-            break
-          case 4:
-            multiArray[1] = []
+            multiArray[1] = this.data.agent_list
             break
         }
         multiIndex[1] = 0
@@ -311,7 +361,6 @@ Page({
       default:
         break
     }
-    console.log('移动了列', multiIndex)
     this.setData({
       multiArray: multiArray,
       multiIndex: multiIndex
